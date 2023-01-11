4336c74c334960ecdf047b7048813e1cec9cf667
mugishasam123 <mugishasamuel42@gmail.com>
2023-01-11 10:38:33 +0200

    Add list

---
+++ exercises/List.java
@@ -0,0 +1,16 @@
+package exercises;
+
+import java.util.*;
+
+public class List {
+    public static void main(String[] args) {
+       
+        LinkedList<Integer> numslist = new LinkedList<Integer>();
+
+        numslist.add(1);
+        numslist.add(1);
+        numslist.add(2);
+        numslist.add(2);
+        System.out.println(numslist);
+    }
+}
